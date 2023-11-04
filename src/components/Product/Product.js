8283de556d111cd19624fc31581d7aae2bc6f248
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-04 19:40:09 +0100

    Add useState for colors and sizes

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,7 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const propTypes = {
   name: PropTypes.string.isRequired,
@@ -12,13 +13,21 @@ const propTypes = {
 };
 
 const Product = (props) => {
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+  const prepareColorClassName = (color) => {
+    return styles[
+      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
+    ];
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img
           className={styles.image}
-          alt='Kodilla shirt'
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`}
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`}
         />
       </div>
       <div>
@@ -30,7 +39,19 @@ const Product = (props) => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li>
+              {props.sizes.map((size) => (
+                <li>
+                  <button
+                    type='button'
+                    className={clsx(size.name === currentSize && styles.active)}
+                    onClick={() => setCurrentSize(size.name)}
+                  >
+                    {size.name}
+                  </button>
+                </li>
+              ))}
+
+              {/* <li>
                 <button type='button' className={styles.active}>
                   S
                 </button>
@@ -43,12 +64,26 @@ const Product = (props) => {
               </li>
               <li>
                 <button type='button'>XL</button>
-              </li>
+              </li> */}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
+              {props.colors.map((color) => (
+                <li>
+                  <button
+                    type='button'
+                    className={clsx(
+                      prepareColorClassName(color),
+                      currentColor === color && styles.active
+                    )}
+                    onClick={() => setCurrentColor(color)}
+                  />
+                </li>
+              ))}
+
+              {/*               
               <li>
                 <button
                   type='button'
@@ -60,7 +95,7 @@ const Product = (props) => {
               </li>
               <li>
                 <button type='button' className={clsx(styles.colorWhite)} />
-              </li>
+              </li> */}
             </ul>
           </div>
           <Button className={styles.button}>
