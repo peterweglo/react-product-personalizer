1f5a9dd9e5c44264989ec773d715fa991cf6eeac
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-04 21:50:44 +0100

    Add ProductImage component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
 
 const propTypes = {
   name: PropTypes.string.isRequired,
@@ -37,13 +38,11 @@ const Product = (props) => {
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt={props.title}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`}
-        />
-      </div>
+      <ProductImage
+        title={props.title}
+        name={props.name}
+        currentColor={currentColor}
+      />
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
